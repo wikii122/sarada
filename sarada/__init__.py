f869c8d43806a89d773008b993f78e4e829a0c28
Wiktor Ślęczka <wikii122@gmail.com>
2021-12-13 17:03:18 +0100

    Bumped version to 0.2.0

--- sarada/__init__.py
+++ sarada/__init__.py
@@ -3,4 +3,6 @@ A small utility aiming to create programatically sound.
 """
 from __future__ import annotations
 
-__version__ = "0.1.0"
+from importlib import metadata
+
+__version__ = metadata.version("sarada")
